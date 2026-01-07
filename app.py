e393aaa0285150a1f6f79a688f189a1a4f04aa57
Adithya Acharya U <138596715+carpeadiam@users.noreply.github.com>
2026-01-07 09:41:55 +0000

    de app

--- app.py
+++ app.py
@@ -449,4 +449,5 @@ def logout():
 # Run App
 # ==========================
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Cloud Run sets PORT
+    app.run(host="0.0.0.0", port=port, debug=False)
